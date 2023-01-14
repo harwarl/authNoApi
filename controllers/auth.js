b8879a75a71c62adcb79badf252b3014dc82b060
harwarl <harwarl87@gmail.com>
2023-01-14 12:42:59 -0800

    fix: signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,9 +5,11 @@ const User = require('../models/user');
 
 //     })
 // }
-// exports.postLogin = (req, res, next) =>{
+exports.postLogin = (req, res, next) =>{
+    const { email, password } = req.body;
+
     
-// }
+}
 // exports.getSignUp = (req, res, next) =>{
 //     res.render('auth/signup', {
         
@@ -15,20 +17,24 @@ const User = require('../models/user');
 // }
 exports.postSignUp = (req, res, next) =>{
     const {email, password, confirmpassword } = req.body;
-    if(password !== confirmpassword) {
-        // return res.redirect('/signup');
-        return res.status(400).json({message: "password do not match"});
-    }
-    bcrypt.hash(password, 10)
-    .then(hashedPassword =>{
-        const user = new User({
-            email: email,
-            password: hashedPassword
-        });
-        return user.save();    
-    })
-    .then(user=>{
-        res.status(200).json({message: user});
+    User.findOne({email: email})
+    .then(user =>{
+        if(user) return res.status(400).json({message: "Email already exists, login instead"});
+        if(password !== confirmpassword) {
+            // return res.redirect('/signup');
+            return res.status(400).json({message: "password do not match"});
+        }
+        bcrypt.hash(password, 10)
+        .then(hashedPassword =>{
+            const user = new User({
+                email: email,
+                password: hashedPassword
+            });
+            return user.save();    
+        })
+        .then(user=>{
+            res.status(200).json({message: user});
+        })
     })
     .catch(err=>console.log(err));
 }
