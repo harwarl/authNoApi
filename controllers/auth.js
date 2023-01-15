acbb015087cd969e5d896b05fa7c5ff08a952c27
harwarl <harwarl87@gmail.com>
2023-01-15 05:27:18 -0800

    added: login and signup pages

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,11 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-// exports.getLogin = (req, res, next) =>{
-//     res.render('auth/login', {
+exports.getLogin = (req, res, next) =>{
+    res.render('auth/login', {
+        pageTitle: 'Login',
+    })
+}
 
-//     })
-// }
 exports.postLogin = (req, res, next) =>{
     const { email, password } = req.body;
     User.findOne({email: email})
@@ -24,19 +25,20 @@ exports.postLogin = (req, res, next) =>{
     .catch(err=>console.log(err));
     
 }
-// exports.getSignUp = (req, res, next) =>{
-//     res.render('auth/signup', {
-        
-//     })
-// }
+exports.getSignUp = (req, res, next) =>{
+    res.render('auth/signup', {
+       pageTitle: "signUp" 
+    })
+}
+
 exports.postSignUp = (req, res, next) =>{
     const {email, password, confirmpassword } = req.body;
     User.findOne({email: email})
     .then(user =>{
         if(user) return res.status(400).json({message: "Email already exists, login instead"});
         if(password !== confirmpassword) {
-            // return res.redirect('/signup');
-            return res.status(400).json({message: "password do not match"});
+            return res.redirect('/signup');
+            // return res.status(400).json({message: "password do not match"});
         }
         bcrypt.hash(password, 10)
         .then(hashedPassword =>{
@@ -47,8 +49,16 @@ exports.postSignUp = (req, res, next) =>{
             return user.save();    
         })
         .then(user=>{
-            res.status(200).json({message: user});
+            res.redirect('/login');
+            // res.status(200).json({message: user});
         })
     })
     .catch(err=>console.log(err));
+}
+
+exports.postLogout = (req, res, next) =>{
+    res.session.destroy(err=>{
+        console.log(err);
+        //redirect to page
+    })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const authController = require('../controllers/auth')
 
 router.post('/signup', authController.postSignUp);
+router.get('/signup', authController.getSignUp);
 router.post('/login', authController.postLogin);
+router.get('/login', authController.getLogin);
+router.post('/logout', authController.postLogout);
 
 module.exports = router;
